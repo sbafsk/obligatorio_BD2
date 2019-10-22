074db850d6ff35cbbf198fa616631226eadbc770
sebahead <razor1836@gmail.com>
2019-10-21 23:10:03 -0300

    Restricciones
    Se crean las restricciones de integridad para todas las tablas, y se corrige los not null en la creacion de tablas.

--- Creacion DB - Tablas.sql
+++ Creacion DB - Tablas.sql
@@ -1,52 +1,79 @@
+-- CREACION DE LA BASE DE DATOS Y SU CORRESPONDIENTES TABLAS
+--
+-- Nota: Las restricciones que no son "not null" o "IDENTITY" 
+-- estan declaradas en el archivo Restricciones.sql
+--
+
+-- DROP DATABASE OBLIGATORIO1BD2
+
 CREATE DATABASE OBLIGATORIO1BD2
 GO
 USE OBLIGATORIO1BD2
 GO
+
+-- tabla artista
 CREATE TABLE artista(artistaId int not null,
                      artistaNombre varchar(30) not null,
 					 artistaFoto varchar(50), 
-					 esNacional numeric(1), 
-					 artistaCantReproducciones numeric(10))
+					 esNacional numeric(1) not null,
+					 artistaCantReproducciones numeric(10) not null) 
 GO
+
+-- tabla album
 CREATE TABLE album(albumId character(5) not null, 
                    albumNombre varchar(30) not null,
 				   artistaId int not null,
-				   albumCantReproducciones numeric(10))
+				   albumCantReproducciones numeric(10) not null)
 GO
+
+-- tabla cancion
 CREATE TABLE cancion(cancionId int identity(1,1) not null,
                      cancionNombre varchar(30) not null,
 					 albumId character(5) not null,
-					 cancionCantReproducciones numeric(10))
+					 cancionCantReproducciones numeric(10) not null)
 GO
+
+-- tabla plan ("plan" es una palabra reservada en SQL, se agrena un digito numerico)
 CREATE TABLE plan1(planId int identity(1,2) not null, 
                    planNombre varchar(30) not null, 
-				   planCosto decimal, 
+				   planCosto decimal not null, 
 				   esPlanVigente numeric(1) not null, 
 				   esRecurrente numeric(1) not null)
 GO
+
+-- tabla usuario
 CREATE TABLE usuario(usuarioId character(20) not null, 
                      usuarioNombre varchar(30) not null, 
-					 usuarioMail varchar(60), 
-					 fechaCreacion date, 
-					 planId int, 
-					 fechaPlanActivoDesde datetime)
+					 usuarioMail varchar(60) not null, 
+					 fechaCreacion date not null, 
+					 planId int not null, 
+					 fechaPlanActivoDesde datetime not null)
 GO
+
+-- tabla compra
 CREATE TABLE compra(compraId int identity(1,1) not null, 
                     usuarioId character(20) not null, 
 					planId int, 
 					fechaGenerada date, 
 					fechaPaga date)
 GO
-CREATE TABLE playList(playListId int identity(1,1) not null, 
+
+-- tabla playList
+CREATE TABLE playList(playListId int IDENTITY(1,1) not null, 
                       playListNombre varchar(30) not null, 
-					  usuarioId character(20) not null, 
-					  playListFechaCreacion date, 
-					  esPlayListCurada numeric(1))
+					  usuarioId character(20), 
+					  playListFechaCreacion date not null, 
+					  esPlayListCurada numeric(1) not null)
 GO
+
+-- tabla playListCancion
 CREATE TABLE playListCancion(playListId int not null,
                              cancionId int not null)
 GO
-CREATE TABLE historialCancion(historialCancionId int identity(1,1) not null, 
+
+-- tabla historialCancion
+-- Nota: La fecha debe guardar hasta los segundos.
+CREATE TABLE historialCancion(historialCancionId int IDENTITY(1,1) not null, 
                               historialCancionFecha datetime not null, 
 							  usuarioId character(20) not null, 
 							  cancionId int not null)
--- Restricciones.sql
+++ Restricciones.sql
@@ -1,4 +1,215 @@
 -- RESTRICCIONES ESTRUCTURALES Y NO ESTRUCTURALES
+--
+-- Nota: Las restricciones "not null" e "INDENTITY" 
+-- estan declaradas junto con la creacion de las tablas.
+--
 
 USE OBLIGATORIO1BD2
 GO
+--
+-- tabla artista
+--
+-- Primary Key
+ALTER TABLE artista
+ADD CONSTRAINT PK_artista 
+	PRIMARY KEY (artistaId)
+
+-- #El campo “esNacional” puede tomar los valores:
+-- - “1” cuando se trata de unartista local. 
+-- - “0” en caso contrario.
+ALTER TABLE artista 
+ADD CONSTRAINT check_esNacional 
+	CHECK (esNacional = 0 OR esNacional = 1)
+
+-- #La cantidad de reproducciones del artista, refiere a la suma de 
+--  todas las reproducciones de todos los temas que el artista tiene en la plataforma.
+
+-- Se debe realizar un calculo con valores de otra tabla para asignarle valor a este campo.
+-- artistaCantReproducciones =
+/*SELECT SUM(C.cancionCantReproducciones) as TotalReproduccionesArtista
+FROM artista Ar, album Al, cancion C
+WHERE	Ar.artistaId = Al.artistaId AND
+		AL.albumId = C.cancionId
+*/
+
+--
+-- tabla album
+--
+-- #Primary Key y Foreign Key
+ALTER TABLE album
+ADD CONSTRAINT PK_album 
+	PRIMARY KEY (albumId),
+
+	CONSTRAINT FK_album_artistaId 
+	FOREIGN KEY (artistaId) 
+	REFERENCES artista(artistaId)
+
+-- #Cada álbum tiene un nombre y pertenece a un artista.
+-- ?????
+
+-- #La cantidad de reproducciones del álbum, refiere a la suma
+--  de todas las reproducciones de los temas del álbum desde la plataforma.
+-- albumCantReproducciones =
+/* SELECT SUM(C.cancionCantReproducciones) as TotalReproduccionesAlbum
+FROM album A, cancion C
+WHERE	A.albumId = C.cancionId
+*/
+
+-- #Los nombres de los álbumes son únicos por artista.
+ALTER TABLE album
+ADD CONSTRAINT uniq_artistaId_albumNombre
+	UNIQUE (artistaId, albumNombre)
+
+
+--
+-- tabla cancion 
+--
+-- #Primary Key y Foreign Key
+ALTER TABLE cancion
+ADD CONSTRAINT PK_cancion 
+	PRIMARY KEY (cancionId),
+
+	CONSTRAINT FK_cancion_albumId 
+	FOREIGN KEY (albumId) 
+	REFERENCES album(albumId)
+
+
+--
+-- tabla plan
+--
+-- #Primary Key
+ALTER TABLE plan1
+ADD CONSTRAINT PK_plan 
+	PRIMARY KEY (planId)
+
+-- #El atributo esPlanVigente tomas el valor: 
+-- - “1” cuando se trata de un plan que actualmente puede ser seleccionado por un usuario.
+-- - “0” si se trata de un plan obsoleto.
+ALTER TABLE plan1 
+ADD CONSTRAINT check_esPlanVigente 
+CHECK (esPlanVigente = 0 OR esPlanVigente = 1)
+
+-- #El atributo esRecurrente toma el valor: 
+-- - “1” cuando el plan requiere de un  pago mensual para mantenerse activo.
+-- - “0” si se trata de un plan de pago puntual.
+ALTER TABLE plan1 
+ADD CONSTRAINT check_esRecurrente 
+CHECK (esRecurrente = 0 OR esRecurrente = 1)
+
+
+--
+-- tabla usuario
+--
+-- #Primary Key y Foreign Key
+ALTER TABLE usuario
+ADD CONSTRAINT PK_usuario 
+	PRIMARY KEY (usuarioId),
+
+	CONSTRAINT FK_usuario_planId 
+	FOREIGN KEY (planId) 
+	REFERENCES plan1(planId)
+
+-- #Tanto el nombre como el mail son únicos en la tabla.
+ALTER TABLE usuario
+ADD CONSTRAINT uniq_usuarioNombre
+	UNIQUE (usuarioNombre),
+
+	CONSTRAINT uniq_usuarioMail
+	UNIQUE (usuarioMail)
+
+
+--
+-- tabla compra
+--
+-- #Primary Key y Foreign Keys.
+ALTER TABLE compra
+ADD CONSTRAINT PK_compra 
+	PRIMARY KEY (compraId),
+
+	CONSTRAINT	FK_compra_usuarioId 
+	FOREIGN KEY (usuarioId) 
+	REFERENCES usuario(usuarioId),
+
+	CONSTRAINT 	FK_compra_planId 
+	FOREIGN KEY (planId) 
+	REFERENCES plan1(planId)
+
+-- #La fecha de paga debe ser mayor o igual a la fecha de generada.
+ALTER TABLE compra 
+ADD CONSTRAINT check_esFechaPagaValida
+CHECK (fechaPaga >= fechaGenerada)
+
+--
+-- tabla playList
+-- 
+-- #Primary Key y Foreign Key
+ALTER TABLE playList
+ADD CONSTRAINT PK_playList 
+	PRIMARY KEY (playListId),
+
+	CONSTRAINT	FK_playList_usuarioId 
+	FOREIGN KEY (usuarioId) 
+	REFERENCES usuario(usuarioId)
+
+-- #Poseen un nombre, único en la tabla.
+ALTER TABLE playList 
+ADD CONSTRAINT uniq_playListNombre
+	UNIQUE (playListNombre)
+
+-- #El atributo “esPlayListCurada” toma el valor
+-- - "1" listas creadas por administradores de la aplicación.
+-- - "0" listas creadas por usuarios.
+ALTER TABLE playList
+ADD CONSTRAINT check_esPlayListCurada
+	CHECK (esPlayListCurada = 1 OR esPlayListCurada = 0)
+
+-- Como resolver lo siguiente ?
+-- IF esPlayListCurada = 0 
+-- usuarioId = not null;
+-- ELSE IF esPlayListCurada = 1 
+-- usuarioId = null;
+
+
+--
+-- tabla playListCancion
+--
+-- #Primary Keys y Foreign Key
+ALTER TABLE playListCancion
+ADD CONSTRAINT PK_playListId_cancionId
+	PRIMARY KEY (playListId, cancionId),
+
+	CONSTRAINT FK_playListCancion_cancionId
+	FOREIGN KEY (cancionId)
+	REFERENCES cancion(cancionId)
+
+--
+-- tabla historiaCancion
+--
+-- #Primary Key y Foreign Keys
+ALTER TABLE historialCancion
+ADD CONSTRAINT PK_historialCancionId
+	PRIMARY KEY (historialCancionId),
+
+	CONSTRAINT FK_historialCancion_usuarioId
+	FOREIGN KEY (usuarioId)
+	REFERENCES usuario(usuarioId),
+
+	CONSTRAINT FK_historialCancion_cancionId
+	FOREIGN KEY (cancionId)
+	REFERENCES cancion(cancionId)
+
+-- #Cada usuario solo puede tener una reproducción por fecha (fecha hora hasta segundos).
+ALTER TABLE historialCancion
+ADD CONSTRAINT uniq_historialCancionFecha
+	UNIQUE (historialCancionFecha)
+
+
+	
+
+	
+
+
+
+
+
+
